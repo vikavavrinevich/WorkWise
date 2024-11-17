69faf9fba302fb6ad827ddafe63bd0c0f580e7fe
vikavavrinevich <viktoriya.vavrinevich@gmail.com>
2024-11-17 15:52:26 +0200

    Initial commit

---
+++ src/test/java/com/coursework/workwise/WorkWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.workwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
